a9ac08b4420a7c95bf47197d470abdd4686abb85
Elton Standerski <eltonstan@yahoo.com.br>
2021-02-03 16:59:21 -0300

    Aula 01 e 02

---
+++ src/main/java/br/com/alura/escolalura/controllers/AlunoController.java
@@ -0,0 +1,40 @@
+package br.com.alura.escolalura.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import br.com.alura.escolalura.models.Aluno;
+import br.com.alura.escolalura.repositories.AlunoRepository;
+
+@Controller
+public class AlunoController {
+
+	@Autowired
+	private AlunoRepository repositorio;
+
+	@GetMapping("/aluno/cadastrar")
+	public String cadastrar(Model model) {
+		model.addAttribute("aluno", new Aluno());
+		return "aluno/cadastrar";
+	}
+
+	@PostMapping("/aluno/salvar")
+	public String salvar(@ModelAttribute Aluno aluno) {
+		repositorio.salvar(aluno); // salvando o aluno
+		return "redirect:/";
+	}
+
+	@GetMapping("/aluno/listar")
+	public String listar(Model model) {
+		List<Aluno> alunos = repositorio.obterTodosAlunos();
+		model.addAttribute("alunos", alunos);
+		return "aluno/listar";
+	}
+
+}
---
+++ src/main/java/br/com/alura/escolalura/models/Nota.java
@@ -0,0 +1,23 @@
+package br.com.alura.escolalura.models;
+
+public class Nota {
+
+	private Double valor;
+
+	public Nota() {
+		super();
+	}
+
+	public Nota(Double valor) {
+		this.valor = valor;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+}
